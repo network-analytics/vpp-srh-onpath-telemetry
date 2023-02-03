2e712eb9e87e4a41f9f540bd3e9add5fc3d2b905
Alex Huang Feng <alex.huang-feng@insa-lyon.fr>
2023-02-03 16:07:16 +0100

    fix: fix env paths

--- dev_jbayaux_ioam_hackathon116/tmux-launch.sh
+++ dev_jbayaux_ioam_hackathon116/tmux-launch.sh
@@ -22,25 +22,25 @@ tmux select-pane -t 2
 tmux split-window -h -p 50
 
 tmux select-pane -t 0
-tmux send-keys 'cd /home/vagrant/vpp-master' C-m
-tmux send-keys 'export STARTUP_CONF=/home/vagrant/Unyte/vpp/dev_jbayaux_ioam_hackathon116/1_vpp.conf' C-m
+tmux send-keys 'cd /home/taahual9/vpp' C-m
+tmux send-keys 'export STARTUP_CONF=/home/taahual9/vpp-srh-onpath-telemetry/dev_jbayaux_ioam_hackathon116/1_vpp.conf' C-m
 
 tmux select-pane -t 1
-tmux send-keys 'cd /home/vagrant/vpp-master' C-m
-tmux send-keys 'export STARTUP_CONF=/home/vagrant/Unyte/vpp/dev_jbayaux_ioam_hackathon116/2_vpp.conf' C-m
+tmux send-keys 'cd /home/taahual9/vpp' C-m
+tmux send-keys 'export STARTUP_CONF=/home/taahual9/vpp-srh-onpath-telemetry/dev_jbayaux_ioam_hackathon116/2_vpp.conf' C-m
 
 tmux select-pane -t 4
-tmux send-keys 'cd /home/vagrant/vpp-master' C-m
-tmux send-keys 'export STARTUP_CONF=/home/vagrant/Unyte/vpp/dev_jbayaux_ioam_hackathon116/3_vpp.conf' C-m
+tmux send-keys 'cd /home/taahual9/vpp' C-m
+tmux send-keys 'export STARTUP_CONF=/home/taahual9/vpp-srh-onpath-telemetry/dev_jbayaux_ioam_hackathon116/3_vpp.conf' C-m
 
 tmux select-pane -t 2
-tmux send-keys 'cd /home/vagrant/Unyte/vpp/dev_jbayaux_ioam_hackathon116' C-m
+tmux send-keys 'cd /home/taahual9/vpp-srh-onpath-telemetry/dev_jbayaux_ioam_hackathon116' C-m
 # tmux send-keys 'sudo python3 sender-udp.py' C-m
 
 tmux select-pane -t 3
-tmux send-keys 'cd /home/vagrant/Unyte/' C-m
+tmux send-keys 'cd /home/taahual9/Unyte/' C-m
 # tmux send-keys 'sudo tcpdump -i vpp2host' C-m
 
 tmux select-pane -t 5
-tmux send-keys 'cd /home/vagrant/Unyte/' C-m
+tmux send-keys 'cd /home/taahual9/Unyte/' C-m
 tmux a -t dev_jbayaux_ioam_hackathon116
